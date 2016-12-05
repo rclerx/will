0fb533d604bd188c6ea99df73c8a6c26d6dd877b
Ryan Gorsuch <ryan.gorsuch@echostar.com>
2016-12-05 12:32:50 -0700

    Adding a better version and an image listener

---
+++ plugins/who.py
@@ -0,0 +1,31 @@
+from will.plugin import WillPlugin
+from will.decorators import respond_to, periodic, hear, randomly, route, rendered_template, require_settings
+import re
+import logging
+import requests
+from will import settings
+
+logger = logging.getLogger(__name__)
+
+
+class WhoPlugin(WillPlugin):
+
+    @respond_to("who are you")
+    def say_bonjour_will(self, message):
+        """who are you: name and serial number"""
+        self.reply(message, "Hello. I am Baymax, your personal healthcare companion. On a scale of one to ten, how would you rate your pain?")
+
+    @respond_to("^http|: http")
+    def pic(self, message):
+        """SDG!"""
+        pattern = re.compile("(http.*(jpg|png|gif))")
+        matches = pattern.search(message['body'])
+        if matches and matches.group():
+            payload = {"image": matches.group()}
+            logger.info("Posting to " + settings.GALLERY_URL + " with body " + str(message['body']) + " and payload " + str(payload))
+            r = requests.post(settings.GALLERY_URL, data=payload)
+            self.reply(message, "Got it!")
+
+    @respond_to("^.")
+    def other(self, message):
+        logger.info("I heard '" + str(message['body']) + "'")
--- will/plugins/admin/version.py
+++ will/plugins/admin/version.py
@@ -1,11 +1,11 @@
 import pkg_resources
 from will.plugin import WillPlugin
 from will.decorators import respond_to
-
+import socket
 
 class VersionPlugin(WillPlugin):
 
     @respond_to("^version$")
     def say_version(self, message):
         version = pkg_resources.get_distribution("will").version
-        self.say("I'm running version %s" % version, message=message)
+        self.say("I'm running version %s from %s" % (version, socket.gethostname()), message=message)
